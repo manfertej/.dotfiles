ab18a414c67d35407ea54937cade30587135f333
Manuel <manfertej@gmail.com>
2025-07-06 15:12:30 +0200

    Included commands for PostgreSQL

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -4,3 +4,6 @@
 alias configzsh="nvim ~/.config/zsh/.zshrc"
 alias confignvim="nvim ~/.config/nvim/init.vim"
 alias configalacritty="nvim ~/.config/alacritty/alacritty.toml"
+alias start-postgresql="systemctl start postgresql.service"
+alias stop-postgresql="systemctl stop postgresql.service"
+alias status-postgresql="systemctl status postgresql.service"
